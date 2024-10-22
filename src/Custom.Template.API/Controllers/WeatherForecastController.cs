1ac2dce53ceb96d60601c8cbcacd24b9e832a41b
SFlrn22 <stoflorin22@gmail.com>
2024-10-22 18:41:40 +0300

    commit

--- src/Custom.Template.API/Controllers/WeatherForecastController.cs
+++ src/Custom.Template.API/Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace API.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            Console.WriteLine("TesT2 Commit + push");
+            Console.WriteLine("TesT3 Commit + push");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
