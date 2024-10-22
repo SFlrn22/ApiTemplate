324791030566424166e18e99432feea503866527
SFlrn22 <stoflorin22@gmail.com>
2024-10-22 18:33:14 +0300

    Test

--- src/Custom.Template.API/Controllers/WeatherForecastController.cs
+++ src/Custom.Template.API/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace API.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine("TesT Commit + push");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
@@ -28,6 +29,7 @@ namespace API.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
         }
     }
 }
